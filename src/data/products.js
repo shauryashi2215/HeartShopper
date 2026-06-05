2672eb46a2e616880940e67a88fd08e9cac99635
shauryashi <shauryashi2211@gmail.com>
2026-06-06 02:11:15 +0530

    final commit

--- src/data/products.js
+++ src/data/products.js
@@ -6,7 +6,7 @@ const products = [
     description: "With the music that echoed once from the strings are now about to be wrapped up on the same fingers they played for, as for the journey we have come this is a token of our love. Made with strings but filled with rhythms of the heart which beats for you.",
     emoji: "💍",
     price: "Your Love",
-    materials: ["Guitar string ball ends", "Made with love by Shaurya"],
+    materials: [ "Guitar string ball ends", "Made with love by Shaurya"],
     rating: 5,
     reviews: 1,
     link: "/product/1"
