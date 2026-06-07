802ed98497af0fb4fc278b37b53161f9c3cba8c6
shauryashi <shauryashi2211@gmail.com>
2026-06-07 23:10:04 +0530

    Email update check

--- src/pages/OrderConfirmation.jsx
+++ src/pages/OrderConfirmation.jsx
@@ -1,40 +1,52 @@
-import PageTransition from '../features/PageTransitions'
+import PageTransition from '../features/PageTransition'
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import emailjs from '@emailjs/browser'
 import '../styles/OrderConfirmation.css'
 
 function OrderConfirmation() {
+
+  useEffect(() => {
+    emailjs.send(
+      'service_y2tqrlf',
+      'template_r3gvvco',
+      {
+        to_name: 'Yashi',
+        to_email: 'beastygamer988@gmail.com',
+        product_name: 'Heart String Ring',
+        delivery_by: 'Shaurya',
+        price: 'Your Love',
+      },
+      '-HAFALzrftSwYyown'
+    )
+  }, [])
+
   return (
     <PageTransition>
       <div className="confirmation-page">
 
-        {/* Checkmark */}
         <div className="confirmation-icon">💍</div>
-
-        {/* Title */}
         <h1 className="confirmation-title">Order Confirmed!</h1>
 
-        {/* Delivery message */}
         <p className="confirmation-delivery">
           Your order is on its way...<br />
-          Estimated Delivery: <span>Right Now </span>
+          Estimated Delivery: <span>As soon as i see you!</span>
         </p>
 
-        {/* Personal message */}
         <div className="confirmation-message">
           <p>
-            Thank you for not just purchasing the ring but to choose me too, this special moment brings us to a commitment of a lifetime.
+            Thank you for not just purchasing the ring but to choose me too,
+            this special moment brings us to a commitment of a lifetime.
           </p>
         </div>
 
-        {/* Order details */}
         <div className="confirmation-details">
           <h3>Order Details</h3>
-          <p>📦 Product: Heart String Ring</p>
+          <p>Product: Heart String Ring</p>
           <p>Delivery by: Shaurya</p>
           <p>Price: Your Love</p>
         </div>
 
-        {/* Back home */}
         <Link to="/" className="back-home-btn">Back to HeartShopper</Link>
 
       </div>
